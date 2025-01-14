5c8827d9752ef1aad61cdd8417cda319a5a580ed
Naviyaoficiall <Chamindumadusanka27@gmail.com>
2025-01-14 20:23:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "NAVIYA-MD=uIFTnbCR#4uw7xLtSewj2xtiGBlvEz6FwwT8-78Pp6Xum_StBJ8M",
-MONGODB: process.env.MONGODB || "mongodb://mongo:RPmQBwWeNBIgZqSWdyimGchxmrZsySkd@autorack.proxy.rlwy.net:49458",
+SESSION_ID: process.env.SESSION_ID || "NAVIYA-MD=OigBzAqS#G9UlFi9qJdhFF6-U8_vhcGcbviZmmsbbeR6duU8uYLA",
+MONGODB: process.env.MONGODB || "mongodb://mongo:gUFbTNjMqxNDZukhBHUFxpIhfNlWgcAm@roundhouse.proxy.rlwy.net:20675",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "8748dc2e"
 };
 
