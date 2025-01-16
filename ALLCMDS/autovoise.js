dfff64cf4c73db2da8ccea04ed8db8a49099ec17
Naviyaoficiall <Chamindumadusanka27@gmail.com>
2025-01-16 18:51:41 +0530

    Create autovoise.js

---
+++ ALLCMDS/autovoise.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const path = require('path');
+const {readEnv} = require('../lib/database')
+const {cmd , commands} = require('../command')
+
+//auto_voice
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../data/autovoice.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            const config = await readEnv();
+            if (config.AUTO_VOICE === 'true') {
+                //if (isOwner) return;        
+                await conn.sendPresenceUpdate('recording', from);
+                await conn.sendMessage(from, { audio: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+            }
+        }
+    }                
+});
+
+//auto sticker 
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../data/autosticker.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            const config = await readEnv();
+            if (config.AUTO_STICKER === 'true') {
+                //if (isOwner) return;        
+                await conn.sendMessage(from,{sticker: { url : data[text]},package: 'yourName'},{ quoted: mek })   
+            
+            }
+        }
+    }                
+});
+
+//auto reply 
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../data/autoreply.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            const config = await readEnv();
+            if (config.AUTO_REPLY === 'true') {
+                //if (isOwner) return;        
+                await m.reply(data[text])
+            
+            }
+        }
+    }                
+});  
+
+
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {       
+ if (config.AUTO_RECORDING === 'true') {
+                await conn.sendPresenceUpdate('recording', from);
+            }
+         } 
+   );
+//always offline
