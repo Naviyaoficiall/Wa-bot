2ce9ef1e74402a12b95a95c62e168da0208fde6a
Naviyaoficiall <Chamindumadusanka27@gmail.com>
2025-02-02 19:06:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -91,22 +91,26 @@ conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://teleg
 })
 conn.ev.on('creds.update', saveCreds)  
 
+//------- *STATUS AUTO REACT* ----------
+
 conn.ev.on('messages.upsert', async(mek) => {
 mek = mek.messages[0]
-if (!mek.message) return	
-mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
 if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
-await conn.readMessages([mek.key])
-}
-if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_LIKE === "true"){
-    const jawadlike = await conn.decodeJid(conn.user.id);
+      await conn.readMessages([mek.key])
+    }
+  if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
+    const emojis = ['🧩', '🍉', '💜', '🌸', '🪴', '💊', '💫', '🍂', '🌟', '🎋', '😶‍🌫️', '🫀', '🧿', '👀', '🤖', '🚩', '🥰', '🗿', '💜', '💙', '🌝', '🖤', '💚'];
+    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
     await conn.sendMessage(mek.key.remoteJid, {
       react: {
-        text: '❤️',
+        text: randomEmoji,
         key: mek.key,
       } 
-    }, { statusJidList: [mek.key.participant, jawadlike] });
+    }, { statusJidList: [mek.key.participant] });
   }
+
+
+//----```© Pink Venom ofc```-----------🗿🤌
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
 const content = JSON.stringify(mek.message)
